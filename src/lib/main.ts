4f620ccb24af4a473889cfd60862dee85f2d2c3b
Tor Arve Stangeland <tor.arve.stangeland@gmail.com>
2021-08-26 07:56:38 +0200

    Check validity before converting

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -5,13 +5,17 @@
   var tkMin = document.getElementById('tk-min') as HTMLInputElement;
 
   function onChangeStandard(evt: Event) {
-    var value = standardMin.value;
-    tkMin.value = !!value ? (+parseFloat(value) / 0.875).toFixed(2) : null;
+    if (standardMin.checkValidity()) {
+      var value = standardMin.value;
+      tkMin.value = !!value ? (+parseFloat(value) / 0.875).toFixed(2) : null;
+    }
   }
   function onChangeTk(evt: Event) {
-    var value = tkMin.value;
-    standardMin.value = !!value ? (parseFloat(value) * 0.875).toFixed(2) : null;
+    if (standardMin.checkValidity()) {
+      var value = tkMin.value;
+      standardMin.value = !!value ? (parseFloat(value) * 0.875).toFixed(2) : null;
   }
+}
 
   standardMin.addEventListener('change', onChangeStandard);
   tkMin.addEventListener('change', onChangeTk);
