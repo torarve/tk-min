ab97bbf81fbcfe1632329ee9a33c1618ec16d9a1
Tor Arve Stangeland <tor.arve.stangeland@gmail.com>
2021-08-26 07:49:24 +0200

    Fix incomplete engineering notation; fex 123e

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -5,18 +5,14 @@
   var tkMin = document.getElementById('tk-min') as HTMLInputElement;
 
   function onChangeStandard(evt: Event) {
-      console.log("onChangeStandard");
     var value = standardMin.value;
-    tkMin.value = !!value ? (+value * 0.875).toFixed(2) : null;
+    tkMin.value = !!value ? (+parseFloat(value) / 0.875).toFixed(2) : null;
   }
   function onChangeTk(evt: Event) {
-      console.log("onChangeTk")
     var value = tkMin.value;
-    standardMin.value = !!value ? (+value / 0.875).toFixed(2) : null;
+    standardMin.value = !!value ? (parseFloat(value) * 0.875).toFixed(2) : null;
   }
 
   standardMin.addEventListener('change', onChangeStandard);
-//   standardMin.addEventListener('keyup', onChangeStandard);
   tkMin.addEventListener('change', onChangeTk);
-//   tkMin.addEventListener('keyup', evt => { console.log(evt); onChangeTk(evt); });
 })();
